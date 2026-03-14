f7aab4a2ba27d6d98a4aa3a73f3da2416f6d58cb
zengqiangz <zengqiangz@tujia.com>
2026-03-14 23:08:21 +0800

    feat: add driver dir and update main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stm32f1xx.h>
-#include "delay.h"
-#include "uart.h"
-
-#define LED_PORT GPIOD
-#define LED_PIN 15
+#include "driver/delay.h"
+#include "driver/uart.h"
 
 int main(void) {
     SysTick_Init();
     uart1_init();
+
     while (1) {
         printf("hello world!\n");
         delayMs(200);
